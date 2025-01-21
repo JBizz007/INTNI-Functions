6aa04650d25adc57c8fbd5a254963ca6f899c8f8
JBE <JBE007@PROTON.ME>
2025-01-22 06:58:45 +1000

    added game id

--- gitGameName.lua
+++ gitGameName.lua
@@ -1,9 +1,15 @@
-local gameName = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
+local gameId = game.PlaceId
+local gameName = game:GetService("MarketplaceService"):GetProductInfo(gameId).Name
 
 local function getGameName()
     return gameName
 end
 
+local function getGameId()
+    return gameId
+end
+
 return {
     GetGameName = getGameName,
+    GetGameId = getGameId,
 }
